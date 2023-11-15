ec77a86fb873f5bdf580167aa49fa291fecf92e1
brunoar86 <ribeiro256@msn.com>
2023-11-15 17:36:43 -0300

    feat: includes structural directives into list-thoughts component

--- memoteca/src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
+++ memoteca/src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListThoughtsComponent implements OnInit {
 
+  listThoughts = [
+    // {
+    //   content: 'Passo informações para o componente filho',
+    //   authorship: 'Componente Pai',
+    //   model: 'modelo1'
+    // },
+    // {
+    //   content: 'Minha propriedade é decorada com @input()',
+    //   authorship: 'Componente Filho',
+    //   model: 'modelo2'
+    // }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
